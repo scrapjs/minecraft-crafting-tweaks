76f7bd4de97075305e91e5fea3fafd5c470833bf
unit-a-user <helixd.2s18@gmail.com>
2022-10-20 12:06:25 +0800

    rename crafting to better_dyeables

---
+++ datapacks/co-extra-better-dyeables/data/better_dyeables/recipes/unreject.js
@@ -0,0 +1,16 @@
+let names = ["banner", "bed", "candle", "concrete", "concrete_powder", "glass", "glass_pane", "glazed_terracotta", "terracotta", "wool"];
+let colors = ["black", "blue", "brown", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow"];
+
+let whatReplace = (name, color, data) => {
+    return data.replaceAll(new RegExp(`\"crafting\:${name}\/(.*?)\"`, "g"),`"better_dyeables:${name}"`);
+}
+
+let fs = require('fs');
+
+names.forEach((name)=>{
+    colors.forEach((color)=>{
+        for (let i=1;i<=8;i++) {
+            fs.writeFileSync(`${name}/${color}/${i}.json`, whatReplace(name, color, fs.readFileSync(`${name}/${color}/${i}.json`, 'utf8')), 'utf8');
+        }
+    });
+});
